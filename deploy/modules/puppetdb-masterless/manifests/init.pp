5e511b12c7f014d73d0081448385336a90f8ecc3
Adrian Lopez <adrianlzt@gmail.com>
2014-04-30 14:24:08 +0200

    Faltaba crear el directorio /etc/puppet/manifests

--- deploy/modules/puppetdb-masterless/manifests/init.pp
+++ deploy/modules/puppetdb-masterless/manifests/init.pp
@@ -21,18 +21,25 @@ class puppetdb-masterless (
     mode => '0644',
   }
   
+  file { '/etc/puppet/manifests' :
+    owner => 'root',
+    group => 'root',
+  }
+
   file { '/etc/puppet/manifests/site.pp':
     ensure => 'link',
     target => '/vagrant/puppetmasterData/site.pp',
     owner => 'root',
     group => 'root',
     mode => '0644',
+    require => File['/etc/puppet/manifests'],
   }
 
   file { '/etc/puppet/manifests/nodes':
     ensure => 'link',
     target => '/vagrant/puppetmasterData/nodes',
     force => true,
+    require => File['/etc/puppet/manifests'],
   }
 
   file { '/etc/puppet/modules':
